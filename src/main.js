f1f228e25d61b03b4d88937e173bc1f6886413c7
Benard Obinna <bernardobinna2@gmail.com>
2019-08-20 04:25:27 +0100

    imported aios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios"
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/main.css";
@@ -11,5 +12,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  axios,
   render: h => h(App)
 }).$mount("#app");
