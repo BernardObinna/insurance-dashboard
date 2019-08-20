9ae1dc8c6259eee901732067373cf7940cde2dc6
Benard Obinna <bernardobinna2@gmail.com>
2019-08-20 05:36:21 +0100

    popullating the data in the state

--- src/store.js
+++ src/store.js
@@ -5,12 +5,20 @@ import axios from "axios";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    applications: {},
+    statistics: {}
+  },
   mutations: {
-    populateData: (state, payload) => {
-      console.log(payload);
-      state = payload;
-      console.log(state);
+    populateApplicationData: (state, payload) => {
+      // console.log(payload.applications);
+      state.applications = payload.applications;
+      // console.log(state.applications);
+    },
+    populateStatisticsData: (state, payload) => {
+      // console.log(payload);
+      state.statistics = payload.statistics;
+      console.log(state.statistics);
     }
   },
   actions: {
@@ -18,24 +26,31 @@ export default new Vuex.Store({
       const token =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjYjg2NWZmMzFhZWRkMzQxNDU0OTQ2YiIsImlhdCI6MTU2NTk0Nzg1NCwiZXhwIjoxNTY1OTU1MDU0fQ.5ujc_dMloqYdyoQN556PhGwIqa1OfGwt06vD5iKu-lg";
 
-      let application = null;
-      let statistics = null;
+      // let application = null;
+      // let statistics = null;
       axios
         .get("http://test.natterbase.com:5050/api/interview/get-applications", {
           headers: { token: token }
         })
         .then(res => {
-          // commit("populateData", res.data);
-          application = res.data;
+          // console.log(res);
+          commit("populateApplicationData", res.data);
         });
       axios
-        .get("http://test.natterbase.com:5050/api/interview/get-applications", {
+        .get("http://test.natterbase.com:5050/api/interview/get-statistics", {
           headers: { token: token }
         })
         .then(res => {
-          // commit("populateData", res.data);
-          statistics = res.data;
+          commit("populateStatisticsData", res.data);
+          // const data = res.data;
+          // return data;
         });
+
+      // const insuranceData = {
+      //   application,
+      //   statistics
+      // };
+      // commit("populateData", application);
     }
   }
 });
