236adee98ecd4ad5d3f91bf80dc52d4a57ca2bc4
Benard Obinna <bernardobinna2@gmail.com>
2019-08-20 03:50:52 +0100

    looped through the imsges

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,13 +1,10 @@
 <template>
   <div class>
     <div class="row">
-      <div class="col mb-md-5 mt-4" v-for="card in cards" :key="card.name">
+      <!-- <p>{{bla}}</p> -->
+      <div class="col mb-md-5 mt-4" v-for="(card,index) in cards" :key="index">
         <div class="card w-100 px-4 py-3" style="width: 18rem;">
-          <img
-            src="@/assets/Insurance-bought-icon.svg"
-            class="card-img-top bg-white h-25 w-25"
-            alt="..."
-          >
+          <img :src="icons[index]" class="card-img-top bg-white h-25 w-25" alt="...">
           <div class="card-body bg-white">
             <h5 class="card-title bg-white text-left">Card title</h5>
             <p
@@ -24,11 +21,30 @@
 export default {
   props: {
     cards: {}
-    // propName: {
-    //   type: Number,
-    //   default:
-    // },
+  },
+
+  data() {
+    return {
+      // bla: "hdhgkrgnkngkngkjg"
+      icons:
+        // {
+        [
+          require("@/assets/Insurance-bought-icon.svg"),
+          require("@/assets/total-amount-insurance-icon.svg"),
+          require("@/assets/insurance-claim-icon.svg"),
+          require("@/assets/amount-of-claims-icon.svg")
+        ]
+      // insuranceBoughtIcon: require("@/assets/Insurance-bought-icon.svg"),
+      // totalAmountIcon: require("@/assets/total-amount-insurance-icon.svg"),
+      // insuranceClaimIcon: require("@/assets/insurance-claim-icon.svg"),
+      // amountClaimIcon: require("@/assets/amount-of-claims-icon.svg")
+      // }
+    };
   }
+  // propName: {
+  //   type: Number,
+  //   default:
+  // },
 };
 </script>
 
