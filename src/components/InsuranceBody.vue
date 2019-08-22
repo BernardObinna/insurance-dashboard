f1c83eb119c1801c97c833f0eb3aae0f26cec5e7
Benard Obinna <bernardobinna2@gmail.com>
2019-08-22 13:32:08 +0100

    adding padding to SN

--- src/components/InsuranceBody.vue
+++ src/components/InsuranceBody.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card shadow-2 w-100" style="width: 18rem;">
-    <div class="card-header text-left">Insurance Applications</div>
-    <div class="d-none d-lg-flex row">
+    <div style="font-size:18px;" class="card-header active-xl text-left">Insurance Applications</div>
+    <div class="d-none pt-3 d-lg-flex row">
       <p class="col-lg-1 pl-4 text-left">S.N</p>
       <p class="col-lg-2">Insurance Type</p>
       <p class="col-lg-2">Amount</p>
